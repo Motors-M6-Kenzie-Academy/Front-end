302f74b7232d9931c775940ca33f3edac1f23a40
larissa-sato <larissasato_@outlook.com>
2023-02-14 14:27:52 -0300

    created ModalImg Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import ProductCard from "./components/ProductCard";
+import ModalImg from "./components/ModalImg";
 import GlobalStyle from "./styles/globalStyles";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <>
       <GlobalStyle />
       {/* <ProductCard /> */}
+      <ModalImg />
     </>
   );
 }
