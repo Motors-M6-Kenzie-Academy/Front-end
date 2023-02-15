95398aaf19e1e652cb83a5d6cbdf6168dbe9f11d
Bruno Azevedo <93809602+brunoacev@users.noreply.github.com>
2023-02-14 21:25:55 -0300

    Feat: Criação de component Input

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,18 @@ import ModalImg from "./components/ModalImg";
 import ProductCardAuction from "./components/ProductCardAuction";
 import Navbar from "./components/Navbar";
 import GlobalStyle from "./styles/globalStyles";
+import InputContainer from "./components/Imputs";
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <Navbar />
+      <InputContainer
+        label="Email"
+        placeHolder="email@email.com"
+        typeInput="email"
+      />
       {/* <ProductCard /> */}
       {/* <ModalImg /> */}
       {/* <ProductCardAuction /> */}
