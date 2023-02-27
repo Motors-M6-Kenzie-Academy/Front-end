9ebb888dbb98b60e068bc9b800e9086fd1c32fec
Bruno Azevedo <93809602+brunoacev@users.noreply.github.com>
2023-02-27 18:05:47 -0300

    Fix: Refatorado Component Navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,21 +1,28 @@
-import { HeaderContainer } from "./styles";
+import {
+  Container,
+  ContainerDesktop,
+  ContainerMobile,
+  Logo,
+  MenuHamburger,
+} from "./styles";
 import LogoImg from "../../assets/LogoImg.svg";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function Navbar() {
-  const handleHamburger = () => {
-    const list = document.querySelector(".nav-bar");
-    list?.classList.toggle("active");
-  };
+  const [user, setUser] = useState({ name: "Samuel Leão" });
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const [isOpenSettingsUser, setIsOpenSettingsUser] = useState(false);
 
-  const handleUser = () => {
-    const list = document.querySelector(".user-settings");
-    list?.classList.toggle("active");
+  const handleSettingsModal = () => {
+    setIsOpenSettingsUser(!isOpenSettingsUser);
+  };
+  const handleHamburger = () => {
+    setIsOpenModal(!isOpenModal);
   };
 
   const sigla: any = [];
   const userName: any = [];
-  const user = { name: "Samuel Leão" };
 
   userName.push(user.name.split(" ")[0]);
   userName.push(user.name.split(" ")[1]);
@@ -24,68 +31,71 @@ export default function Navbar() {
   sigla.push(userName[1].substring(0, 1));
 
   return (
-    <>
-      <HeaderContainer>
-        <div className="logo">
-          <Link to={"/"}>
-            <img src={LogoImg} alt="Logo Motors" />
-          </Link>
-        </div>
-        <div className="hamburger" onClick={() => handleHamburger()}>
-          <div className="line"></div>
-          <div className="line"></div>
-          <div className="line"></div>
-        </div>
-
-        <nav className="nav-bar">
-          <ul>
-            <li>
+    <Container>
+      <Link to={"/"}>
+        <Logo src={LogoImg} alt="Logo Motos" />
+      </Link>
+      <ContainerMobile>
+        {isOpenModal &&
+          (user ? (
+            <nav>
+              <Link to={"/"}>Editar Perfil</Link>
+              <Link to={"/"}>Editar Endereço</Link>
+              <Link to={"/"}>Meus Anúncios</Link>
+              <Link to={"/"}>Excluir Perfil</Link>
+              <Link to={"/"}>Sair</Link>
+            </nav>
+          ) : (
+            <nav>
               <Link to={"/"}>Carros</Link>
-            </li>
-            <li>
               <Link to={"/"}>Motos</Link>
-            </li>
-            <li>
               <Link to={"/"}>Leilão</Link>
-            </li>
-            {/* {user ? (
-              <div className="container-user" onClick={() => handleUser()}>
-                <div className="circle">{sigla}</div>
-                <span className="user-name">{user?.name}</span>
-                <ul className="user-settings">
-                  <li>
-                    <a href="/">Editar Perfil</a>
-                  </li>
-                  <li>
-                    <a href="/">Editar Endereço</a>
-                  </li>
-                  <li>
-                    <a href="/">Minhas Compras</a>
-                  </li>
-                  <li>
-                    <a href="/">Sair</a>
-                  </li>
-                </ul>
+              <div>
+                <Link to={"/signin"}>Fazer Login</Link>
+                <Link to={"/register"}>Cadastrar</Link>
               </div>
-            ) : (
-              <>
-                <li>
-                  <a href="/">Fazer Login</a>
-                </li>
-                <li>
-                  <a href="/">Cadastrar</a>
-                </li>
-              </>
-            )} */}
-            <li>
+            </nav>
+          ))}
+      </ContainerMobile>
+
+      <ContainerDesktop>
+        {user ? (
+          <nav>
+            <Link to={"/"}>Carros</Link>
+            <Link to={"/"}>Motos</Link>
+            <Link to={"/"}>Leilão</Link>
+            <div className="user-info">
+              <span className="user-sigla">{sigla}</span>
+              <span onClick={handleSettingsModal}>{user?.name}</span>
+            </div>
+            {isOpenSettingsUser && (
+              <div id="user-settings-desktop">
+                <Link to={"/"}>Editar Perfil</Link>
+                <Link to={"/"}>Editar Endereço</Link>
+                <Link to={"/"}>Meus Anúncios</Link>
+                <Link to={"/"}>Excluir Perfil</Link>
+                <Link to={"/"}>Sair</Link>
+              </div>
+            )}
+          </nav>
+        ) : (
+          <nav>
+            <Link to={"/"}>Carros</Link>
+            <Link to={"/"}>Motos</Link>
+            <Link to={"/"}>Leilão</Link>
+            <div>
               <Link to={"/signin"}>Fazer Login</Link>
-            </li>
-            <li>
               <Link to={"/register"}>Cadastrar</Link>
-            </li>
-          </ul>
-        </nav>
-      </HeaderContainer>
-    </>
+            </div>
+          </nav>
+        )}
+      </ContainerDesktop>
+
+      <MenuHamburger onClick={() => handleHamburger()}>
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+      </MenuHamburger>
+    </Container>
   );
 }
