67135378799b570cad414967b2daec30ef33832a
Bruno Azevedo <93809602+brunoacev@users.noreply.github.com>
2023-03-03 11:55:33 -0300

    Fix: Correção de alguns bugs na rota ad e context comment

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,22 @@ import { PrivateRout } from "./pages/PrivateRoute";
 import AdsProvider from "./contexts/AdsContexts";
 import UserProvider from "./contexts/UserContexts";
 import AdPage from "./pages/Ad";
+import CommnetsProvider from "./contexts/CommentsContext";
 
 function App() {
   return (
     <UserProvider>
       <AdsProvider>
-        <GlobalStyle />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/ad" element={<AdPage />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/private" element={<PrivateRout />} />
-        </Routes>
+        <CommnetsProvider>
+          <GlobalStyle />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/ad" element={<AdPage />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/private" element={<PrivateRout />} />
+          </Routes>
+        </CommnetsProvider>
       </AdsProvider>
     </UserProvider>
   );
--- src/contexts/CommentsContext.tsx
+++ src/contexts/CommentsContext.tsx
@@ -1,70 +1,80 @@
-import { createContext, ReactNode, useState, useEffect } from "react";
+import {
+  createContext,
+  ReactNode,
+  useState,
+  useEffect,
+  Dispatch,
+  SetStateAction,
+} from "react";
 import { IAds } from "../interfaces/Ads";
 import { ICommentsRequest, ICommentsResponse } from "../interfaces/Comments";
-import api, {RequestAPI} from "../services/api";
+import api, { RequestAPI } from "../services/api";
 
-
-interface CommentsContextData{
-    onSubmitComments: (data: ICommentsRequest) => void;
-    listComments: ICommentsResponse[];
-    setListComments: React.Dispatch<React.SetStateAction<ICommentsResponse[]>>;
-    commentsApi: ICommentsResponse;
-    setCommentsApi: React.Dispatch<React.SetStateAction<ICommentsResponse>>;
-    setAdsId: React.Dispatch<React.SetStateAction<IAds>>
+interface CommentsContextData {
+  onSubmitComments: (data: ICommentsRequest) => void;
+  listComments: ICommentsResponse[];
+  setListComments: React.Dispatch<React.SetStateAction<ICommentsResponse[]>>;
+  commentsApi: ICommentsResponse;
+  setCommentsApi: React.Dispatch<React.SetStateAction<ICommentsResponse>>;
+  setAdsId: Dispatch<SetStateAction<string>>;
 }
 
 export const CommentsContext = createContext<CommentsContextData>(
-    {} as CommentsContextData
+  {} as CommentsContextData
 );
 
 export interface ICommentsProvierProps {
-    children: ReactNode;
-  }
+  children: ReactNode;
+}
 
-const CommnetsProvider = ({children}: ICommentsProvierProps) => {
-    const [listComments, setListComments] = useState<ICommentsResponse[]>([])
-    const [commentsApi, setCommentsApi] = useState<ICommentsResponse>({} as ICommentsResponse)
-    const [adsId, setAdsId] = useState<IAds>({} as IAds)
+const CommnetsProvider = ({ children }: ICommentsProvierProps) => {
+  const [listComments, setListComments] = useState<ICommentsResponse[]>([]);
+  const [commentsApi, setCommentsApi] = useState<ICommentsResponse>(
+    {} as ICommentsResponse
+  );
+  const [adsId, setAdsId] = useState<string>("");
 
-    const getComments = async () => {
-        await api
-          .get("/ads")
-          .then((res) => {
-            setListComments(res.data);
-          })
-          .catch((err) => console.log(err));
-      };
-    
-      useEffect(() => {
-        getComments();
-      }, []);
+  const getComments = async () => {
+    await api
+      .get("/ads")
+      .then((res) => {
+        setListComments(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
 
-    const onSubmitComments = async(data: ICommentsRequest) => {
-        const response = await RequestAPI("comments/", "post", data, `${adsId}`)
-        setListComments((oldComments) => [...oldComments, response])
-        setCommentsApi(response)
+  useEffect(() => {
+    getComments();
+  }, []);
 
-        // api
-        // .post("/comments/", data)
-        // .then((res) => {
-        //     setListComments((oldComments) => [...oldComments, res.data])
-        //     setCommentsApi(res.data)
-        // })
-        // .catch((err) => console.log(err));
-    };
+  const onSubmitComments = async (data: ICommentsRequest) => {
+    const response = await RequestAPI("comments/", "post", data, `${adsId}`);
+    setListComments((oldComments) => [...oldComments, response]);
+    setCommentsApi(response);
 
-    return (
-        <CommentsContext.Provider value={{
-            onSubmitComments,
-            listComments,
-            setListComments,
-            commentsApi,
-            setCommentsApi,
-            setAdsId
-        }}>
-            {children}
-        </CommentsContext.Provider>
-    );
+    // api
+    // .post("/comments/", data)
+    // .then((res) => {
+    //     setListComments((oldComments) => [...oldComments, res.data])
+    //     setCommentsApi(res.data)
+    // })
+    // .catch((err) => console.log(err));
+  };
+
+  return (
+    <CommentsContext.Provider
+      value={{
+        onSubmitComments,
+        listComments,
+        setListComments,
+        commentsApi,
+        setCommentsApi,
+        setAdsId,
+      }}
+    >
+      {children}
+    </CommentsContext.Provider>
+  );
 };
 
-export default CommnetsProvider
+export default CommnetsProvider;
